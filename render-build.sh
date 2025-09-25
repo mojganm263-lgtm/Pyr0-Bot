5b47e5ca09e37d318c8974e57b8c64bd75f7938e
mojganm263-lgtm <mojganm263@gmail.com>
2025-09-25 04:35:39 +0100

    render-build.sh

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Uninstall any old discord packages
-pip uninstall -y discord py-cord discord.py
-
-# Install required packages
-pip install discord.py==2.2.2
-pip install transformers torch flask matplotlib
